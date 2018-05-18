923b678eed0796ed7d0da14cebcbf76a962b4a4c
DESKTOP-IM80HHS\Fusiu <fusiu_online@163.com>
2018-05-18 21:06:24 +0800

    weather-collection-server 创建

---
+++ msa-weather/weather-collection-server/src/main/java/com/msa/weather/collection/server/WeatherCollectionServerApplication.java
@@ -0,0 +1,12 @@
+package com.msa.weather.collection.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCollectionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherCollectionServerApplication.class, args);
+	}
+}
